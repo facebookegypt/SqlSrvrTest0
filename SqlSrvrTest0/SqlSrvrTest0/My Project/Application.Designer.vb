b726d0610b520085ac84d9c12bc565ea8c79bed6
evry1falls <speechless_man@hotmail.com>
2023-04-27 02:53:17 +0200

    Login Form

--- SqlSrvrTest0/SqlSrvrTest0/My Project/Application.Designer.vb
+++ SqlSrvrTest0/SqlSrvrTest0/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.SqlSrvrTest0.Form1
+            Me.MainForm = Global.SqlSrvrTest0.LoginForm1
         End Sub
     End Class
 End Namespace
--- SqlSrvrTest0/SqlSrvrTest0/My Project/Resources.Designer.vb
+++ SqlSrvrTest0/SqlSrvrTest0/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property login_icon_3058() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("login_icon_3058", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Refresh() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Refresh", resourceCulture)
